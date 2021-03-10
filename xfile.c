9d8c1dd9336c0718f56143b6f377e89a11c45d9c
zhouyuming <1006129869@qq.com>
2021-03-10 10:57:41 +0800

    update xfile

--- xfile.c
+++ xfile.c
@@ -1,1 +1,22 @@
+#include "xfile.h"
 
+#ifdef __cplusplus
+#if __cplusplus
+extern "C"{
+#endif
+#endif /* __cplusplus */
+
+#ifdef __XFILE_SVRSIDE__
+
+int main(int argc, char **argv)
+{
+    printf ("xfile - by zhouyuming, build on %s %s\r\n", __DATE__, __TIME__);
+}
+
+#endif /* __XFILE_SVRSIDE__ */
+
+#ifdef __cplusplus
+#if __cplusplus
+}
+#endif
+#endif /* __cplusplus */
--- xfile.h
+++ xfile.h
@@ -1,1 +1,19 @@
+/*
+ * xfile.h
+ *
+ */
 
+#ifndef __XFILE_H__
+#define __XFILE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __XFILE_H__ */
